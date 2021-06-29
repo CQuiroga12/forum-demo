ab619efa6e6f5a35189905a29922bd3041442c21
CQuiroga12 <77950094+CQuiroga12@users.noreply.github.com>
2021-06-29 14:21:18 -0400

    Forum Board Proof of Concept

---
+++ public/app.js
@@ -0,0 +1,52 @@
+let textPost = document.getElementById("text-post");
+let uploadButton = document.getElementById("js-fb-data");
+let postIndex = 0;
+
+function getPostIndex() {
+  firebase
+    .database()
+    .ref("PostIndex")
+    .on("value", (snapshot) => {
+      postIndex = snapshot.val();
+      console.log(snapshot.val());
+    });
+}
+
+uploadButton.addEventListener("click", () => {
+  firebase
+    .database()
+    .ref("posts/" + postIndex)
+    .set({
+      Post: textPost.value,
+      Index: postIndex,
+    });
+  postIndex++;
+  firebase.database().ref("PostIndex").set(postIndex);
+  getPostIndex();
+});
+
+uploadButton.addEventListener("click", addPosts);
+
+let renderIndex = 0;
+let postArea = document.querySelector(".posts");
+function addPosts() {
+  let targetData = firebase.database().ref("posts").orderByKey();
+  targetData.once("value").then((snapshot) => {
+    snapshot.forEach(renderPosts);
+  });
+}
+
+function renderPosts() {
+  firebase
+    .database()
+    .ref("posts/" + renderIndex)
+    .on("value", (snapshot) => {
+      let newPost = document.createElement("p");
+      newPost.textContent = snapshot.val().Post;
+      postArea.appendChild(newPost);
+    });
+  renderIndex++;
+}
+
+document.addEventListener("DOMContentLoaded", addPosts);
+document.addEventListener("DOMContentLoaded", getPostIndex);
